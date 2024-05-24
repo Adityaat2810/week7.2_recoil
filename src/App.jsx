5889e4a6b1c5e85573b50b0431a22bd01bcd9271
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-05-24 22:46:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    
+    <div>
+      <button>Home</button>
+      <button>My Network</button>
+      <button>Jobs</button>
+      <button>Messaging</button>
+      <button>Notification</button>
+      <button>Me</button>
+
+
+    </div>
+  )
+}
+
+export default App
