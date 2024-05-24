155d797341184435a93d1497eaf069609363655f
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-05-24 23:03:56 +0530

    recoil atoms and useRecoilValue

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
+import { useRecoilValue } from 'recoil'
+import { jobsAtom, messagingAtom, networkAtom, notificationAtom } from './atoms'
+//never forget to wrap inside RecoilRoot
 
 const App = () => {
+  
+  const networkNotifiactionCount = useRecoilValue(networkAtom)
+  const jobsNotificationCount =useRecoilValue(jobsAtom)
+  const messagingNotificationValue = useRecoilValue(messagingAtom)
+  const notificationValue = useRecoilValue(notificationAtom)
+
   return (
-    
+
     <div>
+
+
+
       <button>Home</button>
-      <button>My Network</button>
-      <button>Jobs</button>
-      <button>Messaging</button>
-      <button>Notification</button>
+      <button>My Network {networkNotifiactionCount}</button>
+      <button>Jobs {jobsNotificationCount}</button>
+      <button>Messaging {messagingNotificationValue}</button>
+      <button>Notification {notificationValue}</button>
       <button>Me</button>
 
 
